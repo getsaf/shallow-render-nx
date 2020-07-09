54eca2141b95acbdefdcb55c52d7b24f4c3a5616
Brandon Domingue <getsaf@gmail.com>
2020-07-09 12:35:43 -0400

    Use shallow-render and test bindings

--- apps/shallow-render-nx/src/app/app.component.spec.ts
+++ apps/shallow-render-nx/src/app/app.component.spec.ts
@@ -1,31 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {AppModule} from './app.module';
+import { Shallow } from 'shallow-render';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-    }).compileComponents();
-  }));
+  let shallow: Shallow<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    shallow = new Shallow(AppComponent, AppModule);
   });
 
-  it(`should have as title 'shallow-render-nx'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('shallow-render-nx');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to shallow-render-nx!'
-    );
+  it(`should work with bind 'shallow-render-nx'`, async () => {
+    const {find} = await shallow.render({bind: {myInput: 'My Test Input'}});
+    expect(find('.myInput').nativeElement.textContent).toBe('My Test Input');
   });
 });
